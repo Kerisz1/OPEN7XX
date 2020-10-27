7a6ce444a07aba2f04791c95e97e3bd631cdb46e
IrekPC <IrekPC@192.168.0.248>
2020-10-27 05:45:10 +0100

    12

--- Test_OPEN7_1/Drivers/BSP/stm32746i_OPEN7XXI_qspi.h
+++ Test_OPEN7_1/Drivers/BSP/stm32746i_OPEN7XXI_qspi.h
@@ -76,10 +76,10 @@
 #define QSPI_CLK_DISABLE()         __HAL_RCC_QSPI_CLK_DISABLE()
 #define QSPI_CS_GPIO_CLK_ENABLE()  __HAL_RCC_GPIOB_CLK_ENABLE()
 #define QSPI_CLK_GPIO_CLK_ENABLE() __HAL_RCC_GPIOB_CLK_ENABLE()
-#define QSPI_D0_GPIO_CLK_ENABLE()  __HAL_RCC_GPIOD_CLK_ENABLE()
-#define QSPI_D1_GPIO_CLK_ENABLE()  __HAL_RCC_GPIOD_CLK_ENABLE()
-#define QSPI_D2_GPIO_CLK_ENABLE()  __HAL_RCC_GPIOE_CLK_ENABLE()
-#define QSPI_D3_GPIO_CLK_ENABLE()  __HAL_RCC_GPIOD_CLK_ENABLE()
+#define QSPI_D0_GPIO_CLK_ENABLE()  __HAL_RCC_GPIOF_CLK_ENABLE()
+#define QSPI_D1_GPIO_CLK_ENABLE()  __HAL_RCC_GPIOF_CLK_ENABLE()
+#define QSPI_D2_GPIO_CLK_ENABLE()  __HAL_RCC_GPIOF_CLK_ENABLE()
+#define QSPI_D3_GPIO_CLK_ENABLE()  __HAL_RCC_GPIOF_CLK_ENABLE()
 
 #define QSPI_FORCE_RESET()         __HAL_RCC_QSPI_FORCE_RESET()
 #define QSPI_RELEASE_RESET()       __HAL_RCC_QSPI_RELEASE_RESET()
@@ -89,14 +89,14 @@
 #define QSPI_CS_GPIO_PORT          GPIOB
 #define QSPI_CLK_PIN               GPIO_PIN_2
 #define QSPI_CLK_GPIO_PORT         GPIOB
-#define QSPI_D0_PIN                GPIO_PIN_11
-#define QSPI_D0_GPIO_PORT          GPIOD
-#define QSPI_D1_PIN                GPIO_PIN_12
-#define QSPI_D1_GPIO_PORT          GPIOD
-#define QSPI_D2_PIN                GPIO_PIN_2
-#define QSPI_D2_GPIO_PORT          GPIOE
-#define QSPI_D3_PIN                GPIO_PIN_13
-#define QSPI_D3_GPIO_PORT          GPIOD
+#define QSPI_D0_PIN                GPIO_PIN_8
+#define QSPI_D0_GPIO_PORT          GPIOF
+#define QSPI_D1_PIN                GPIO_PIN_9
+#define QSPI_D1_GPIO_PORT          GPIOF
+#define QSPI_D2_PIN                GPIO_PIN_7
+#define QSPI_D2_GPIO_PORT          GPIOF
+#define QSPI_D3_PIN                GPIO_PIN_6
+#define QSPI_D3_GPIO_PORT          GPIOF
 
 /* W25Q128FV13EF840E Micron memory */
 /* Size of the flash */
